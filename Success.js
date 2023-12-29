399064742cc2a5c7c17da21b3b6906a643cb5476
717821p206 <137895245+717821p206@users.noreply.github.com>
2023-12-29 07:17:29 +0530

    Add files via upload

--- Success.js
+++ Success.js
@@ -1,10 +1,15 @@
 import React from "react";
 import './Success.css'
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 function Success(){
 return(
     <div className="success">
+        <div>
     <i class=" icon bi-check-circle-fill "  ></i>
     <h1 >Your order is Confirmed</h1>
+    </div>
+    <Link to='/DisplayReport'><Button className="reportbtn m-5 p-2 fs-2">Report</Button></Link>
     </div>
 )
 }
